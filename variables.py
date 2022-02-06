b509d13c2953f5e31c38e75ab887203fca684eea
Allan <98925545+130-vac@users.noreply.github.com>
2022-02-05 21:59:18 -0600

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,8 +1,9 @@
-z = 20
-
-x = 10
-
-y = 3
-
-
 resultado = z + x * 365 - y * 52
+
+monedas_encontradas 20
+
+monedas_robadas 3
+
+monedas_magicas 10
+
+monedas_encontradas + monedas_magicas = 30*365 - monedas_robadas * 52
